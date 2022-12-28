885756c79a25284449269373faed77fbdeb054f1
DivDE <divde@musiciel.fr>
2022-12-28 18:27:05 +0100

    Adding a logo

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# Application question
+# Question
+
+![Logo](public/android-chrome-192x192.png)
 
 Cette application permet d'écrire une question avec un ensemble de réponses possibles (éventuellement munies de coefficients de probabilité),
 et de cliquer sur "Réponse" pour obtenir une réponse aléatoire.
+
 Un bouton "Simulation" permet d'exécuter la procédure un grand nombre de fois, et d'obtenir pour chaque réponse possible le nombre d'occurrences.
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,29 @@ import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
 	plugins: [
 		svelte(),
-		VitePWA({ registerType: "autoUpdate", injectRegister: null })
+		VitePWA({
+			registerType: "autoUpdate",
+			injectRegister: null,
+			manifest: {
+				name: "Question",
+				description: "",
+				icons: [
+					{
+						src: "/android-chrome-192x192.png",
+						sizes: "192x192",
+						type: "image/png"
+					},
+					{
+						src: "/android-chrome-512x512.png",
+						sizes: "512x512",
+						type: "image/png"
+					}
+				],
+				theme_color: "#ffffff",
+				background_color: "#ffffff",
+				display: "standalone",
+				lang: "fr"
+			}
+		})
 	]
 });
