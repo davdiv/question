1e7c1a99bd6ca7a7be8802a4880f27ab45555a59
DivDE <divde@musicociel.fr>
2024-11-01 16:31:28 +0100

    Upgrading android version

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,17 +3,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig = {
 	appId: "fr.shamayim.question",
 	appName: "Question",
-	webDir: "dist",
-	bundledWebRuntime: false,
-	android: {
-		buildOptions: {
-			keystorePath: "keys/keys.keystore",
-			keystoreAlias: "keys",
-			keystorePassword: process.env.KEYSTORE_PASS,
-			keystoreAliasPassword: process.env.KEYSTORE_ALIAS_PASS,
-			releaseType: "APK"
-		}
-	}
+	webDir: "dist"
 };
 
 export default config;
